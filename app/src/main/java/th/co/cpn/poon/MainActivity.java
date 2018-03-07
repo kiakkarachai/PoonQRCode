d39cca472101e89a3ee33a457b0f73a212c902ee
spoon <kiakkarachai@gmail.com>
2018-03-07 15:34:25 +0700

    Create MainFragment

--- app/src/main/java/th/co/cpn/poon/MainActivity.java
+++ app/src/main/java/th/co/cpn/poon/MainActivity.java
@@ -3,11 +3,22 @@ package th.co.cpn.poon;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import th.co.cpn.poon.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment to Activity
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment()).commit();
+        }
+
+    } // Main Method
+
+}  // Main Class
