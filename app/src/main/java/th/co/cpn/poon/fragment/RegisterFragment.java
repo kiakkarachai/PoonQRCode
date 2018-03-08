d5de5320d271bd9ff1a9c3feafd9f7e54fe45660
spoon <kiakkarachai@gmail.com>
2018-03-08 11:51:16 +0700

    Check Space and Alert

--- app/src/main/java/th/co/cpn/poon/fragment/RegisterFragment.java
+++ app/src/main/java/th/co/cpn/poon/fragment/RegisterFragment.java
@@ -5,11 +5,16 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import th.co.cpn.poon.MainActivity;
 import th.co.cpn.poon.R;
+import th.co.cpn.poon.utility.MyAlert;
 
 /**
  * Created by kiakkarachai on 08/03/2018.
@@ -24,10 +29,55 @@ public class RegisterFragment extends Fragment{
 //        Create Toolbar
         createToolbar();
 
-
     } //Main Method
 
+    //กด Alt + Insert
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.itemUpload) {
+            uploadValueToServer();
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void uploadValueToServer() {
+//        Get Value From Edit Text
+        EditText nameEditText = getView().findViewById(R.id.edtName);
+        EditText userEditText = getView().findViewById(R.id.edtUser);
+        EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+        String nameString = nameEditText.getText().toString().trim();
+        String userString = userEditText.getText().toString().trim();
+        String passwordString = passwordEditText.getText().toString().trim();
+
+//        Check Space
+        if (nameString.isEmpty() || userString.isEmpty() ||passwordString.isEmpty() ) {
+//            have space
+            MyAlert myAlert = new MyAlert(getActivity());
+            myAlert.myDialog("Have Dpace","Please Fill All Blank");
+
+        } else {
+//            no space
+
+        } // if
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_register, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
     private void createToolbar() {
+
+        setHasOptionsMenu(true);
+
         Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
         ((MainActivity)getActivity()).setSupportActionBar(toolbar);
 
---
+++ app/src/main/java/th/co/cpn/poon/utility/MyAlert.java
@@ -0,0 +1,39 @@
+package th.co.cpn.poon.utility;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+
+import th.co.cpn.poon.R;
+
+/**
+ * Created by kiakkarachai on 08/03/2018.
+ */
+
+public class MyAlert {
+
+    private Context context;
+
+    public MyAlert(Context context) {
+        this.context = context;
+    }
+
+    public void  myDialog(String titleString, String messageString) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.ic_action_alert);
+        builder.setTitle(titleString);
+        builder.setMessage(messageString);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+
+    }
+
+
+}  // Main Class
