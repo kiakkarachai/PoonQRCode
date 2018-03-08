2d6dc7ba87b7c7565297c5cc51efe479d4b4ca7e
spoon <kiakkarachai@gmail.com>
2018-03-08 10:34:55 +0700

    Replate Fragment

--- app/src/main/java/th/co/cpn/poon/fragment/RegisterFragment.java
+++ app/src/main/java/th/co/cpn/poon/fragment/RegisterFragment.java
@@ -3,10 +3,12 @@ package th.co.cpn.poon.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import th.co.cpn.poon.MainActivity;
 import th.co.cpn.poon.R;
 
 /**
@@ -14,6 +16,36 @@ import th.co.cpn.poon.R;
  */
 
 public class RegisterFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        createToolbar();
+
+
+    } //Main Method
+
+    private void createToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+//        Setup Title and SubTitle
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.register));
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle(getString(R.string.message_register));
+
+//        Setup Navigator
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+    }
 
     @Nullable
     @Override
