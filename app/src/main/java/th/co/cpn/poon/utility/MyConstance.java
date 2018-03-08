4aeadfb33ec857ef37f09dc92bd4f5e187e24157
spoon <kiakkarachai@gmail.com>
2018-03-08 14:47:05 +0700

    Sign Up Sccuess

---
+++ app/src/main/java/th/co/cpn/poon/utility/MyConstance.java
@@ -0,0 +1,14 @@
+package th.co.cpn.poon.utility;
+
+/**
+ * Created by kiakkarachai on 08/03/2018.
+ */
+
+public class MyConstance {
+
+    private String urlAddUser = "http://androidthai.in.th/cent/addDataPoon.php";
+
+    public String getUrlAddUser() {
+        return urlAddUser;
+    }
+}
