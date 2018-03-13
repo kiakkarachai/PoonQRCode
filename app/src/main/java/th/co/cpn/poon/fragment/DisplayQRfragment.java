3785ee237d35337100876272a68bdfa640cd6402
spoon <kiakkarachai@gmail.com>
2018-03-13 15:22:26 +0700

    Exit OK

--- app/src/main/java/th/co/cpn/poon/fragment/DisplayQRfragment.java
+++ app/src/main/java/th/co/cpn/poon/fragment/DisplayQRfragment.java
@@ -3,13 +3,19 @@ package th.co.cpn.poon.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import th.co.cpn.poon.R;
+import th.co.cpn.poon.utility.GetFoodWhereQRcode;
+import th.co.cpn.poon.utility.MyConstance;
 
 /**
  * Created by kiakkarachai on 13/03/2018.
@@ -64,7 +70,46 @@ public class DisplayQRfragment extends Fragment{
             @Override
             public void onClick(View view) {
 
-            }
+                String tag = "13MarchV1";
+
+                MyConstance myConstance = new MyConstance();
+                String[] columnString = myConstance.getColumnFoodString();
+                String[] valueStrings = new String[columnString.length];
+
+                try {
+
+                    GetFoodWhereQRcode getFoodWhereQRcode = new GetFoodWhereQRcode(getActivity());
+                    getFoodWhereQRcode.execute(qrScanString, myConstance.getUrlGetFoodWhereQRcode());
+
+                    String jsonString = getFoodWhereQRcode.get();
+                    Log.wtf(tag, "JSON ==>" + jsonString);
+
+                    JSONArray jsonArray = new JSONArray(jsonString);
+                    JSONObject jsonObject = jsonArray.getJSONObject(0);
+
+                    for (int i = 0; i < columnString.length; i += 1) {
+
+                        valueStrings[i] = jsonObject.getString(columnString[i]);
+
+                    } // for
+
+                    getActivity().getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.contentServiceFragment, DetailFragment.detailInstance(
+                                    valueStrings[2],
+                                    valueStrings[5],
+                                    valueStrings[1],
+                                    valueStrings[3],
+                                    valueStrings[4]))
+                            .addToBackStack(null).commit();
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+
+                }
+
+            } // On Click
         });
     }
 
--- app/src/main/java/th/co/cpn/poon/utility/MyConstance.java
+++ app/src/main/java/th/co/cpn/poon/utility/MyConstance.java
@@ -5,12 +5,23 @@ package th.co.cpn.poon.utility;
  */
 
 public class MyConstance {
+
+    private String urlGetFoodWhereQRcode = "http://androidthai.in.th/cent/getFoodWhereQRcodeMaster.php";
     private String urlReadAllFood = "http://androidthai.in.th/cent/getAllFood.php";
     private String urlAddUser = "http://androidthai.in.th/cent/addDataPoon.php";
     private String urlReadAllUser = "http://androidthai.in.th/cent/getAllDataPoon.php";
 
     private String[] columnUserTableStrings = new String[]{"id", "Name", "User", "Password"};
+    private String[] columnFoodString = new String[]{"id", "Category", "NameFood", "Price", "Detail", "ImagePath", "QRcode"};
 
+
+    public String[] getColumnFoodString() {
+        return columnFoodString;
+    }
+
+    public String getUrlGetFoodWhereQRcode() {
+        return urlGetFoodWhereQRcode;
+    }
 
     public String getUrlReadAllFood() {
         return urlReadAllFood;
